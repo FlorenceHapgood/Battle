ac6399815715ac2163e0158a633482397597a988
Florence Hapgood <flohapgood@icloud.com>
2018-09-19 16:02:44 +0100

    Second commit?

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,10 @@
+feature "Enter Names" do
+  scenario "Submits a name in a form" do
+    visit ('/')
+    fill_in :name_1, with: "Emma"
+    fill_in :name_2, with: "Florence"
+    click_button "Submit"
+
+    expect(page).to have_content ("Emma vs. Florence")
+  end
+end
